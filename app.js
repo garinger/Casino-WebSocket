d3865fd752a9f15016fa94a7bf87a0e92343353f
Jackson Garinger <jackson.garinger@gmail.com>
2024-03-06 18:57:44 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+import { Server } from "socket.io";
+import { format } from "date-fns";
+
+const io = new Server(3000, { cors: { origin: ["http://localhost:5173"] } });
+
+const millisecondsBetweenSpins = 12_000;
+const spinDurationMilliseconds = 6_000;
+const spinOffsetAbsoluteValue = 15;
+const gameState = {
+  outcome: null,
+  spinning: false,
+  spinStart: null,
+  spinEnd: null,
+  spinOffset: null,
+  nextSpin: new Date(Date.now() + millisecondsBetweenSpins),
+  pastGames: [],
+};
+
+let spinCount = 0;
+
+const getOutcome = () => Math.floor(Math.random() * 15);
+const getSpinOffset = (abs) => Math.floor(Math.random() * (abs * 2 + 1)) - abs;
+const emitGameState = () => {
+  io.emit("gameState", gameState);
+  console.log(
+    "[Game State]",
+    "outcome:",
+    gameState.outcome,
+    "spinning:",
+    gameState.spinning,
+    "spinOffset:",
+    gameState.spinOffset,
+    "spinStart:",
+    format(gameState.spinStart || new Date(0), "h:mm:ss a"),
+    "spinEnd:",
+    format(gameState.spinEnd || new Date(0), "h:mm:ss a"),
+    "nextSpin:",
+    format(gameState.nextSpin, "h:mm:ss a"),
+    "pastGames:",
+    gameState.pastGames.length
+  );
+};
+
+io.on("connection", (socket) => {
+  console.log("[Connection] User connected");
+  emitGameState();
+});
+
+const spin = () => {
+  gameState.outcome = getOutcome();
+  gameState.spinning = true;
+  gameState.spinStart = new Date();
+  gameState.spinEnd = new Date(Date.now() + spinDurationMilliseconds);
+  gameState.spinOffset = getSpinOffset(spinOffsetAbsoluteValue);
+  gameState.nextSpin = new Date(Date.now() + millisecondsBetweenSpins);
+
+  spinCount++;
+  emitGameState();
+};
+
+setInterval(() => {
+  spin();
+
+  setTimeout(() => {
+    gameState.pastGames.push({
+      id: spinCount,
+      outcome: gameState.outcome,
+      date: gameState.spinStart,
+    });
+    gameState.spinning = false;
+    emitGameState();
+  }, spinDurationMilliseconds);
+}, millisecondsBetweenSpins);
